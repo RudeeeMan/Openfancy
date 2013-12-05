e27944dd5d079df9edd6fa3b01b09405008ca7f4
RudeeeMan <fboegge@gmail.com>
2013-12-05 19:08:50 +0100

    Initial Commit

---
+++ Openfancy App/App/App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by Florian Bögge on 05.12.13.
+//  Copyright (c) 2013 Florian Bögge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Openfancy App/App/App/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  App
+//
+//  Created by Florian Bögge on 05.12.13.
+//  Copyright (c) 2013 Florian Bögge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
